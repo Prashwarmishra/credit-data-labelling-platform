d83de37d9376bbadc7fc1fa667aa9ce85e25f5d7
Prashwar <prashwar.mishra@myparkplus.com>
2024-04-21 08:59:57 +0530

    install react-router-dom, sass, and fontawesome packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import s from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.root}>
+      <div className={s.child} />
+      <FontAwesomeIcon icon={faPenNib} />
     </div>
   );
 }
